22c0fc8410e87e1fd1295fbbb3c0eb6c06725bea
Jesse Gibson <JesseTheGibson@gmail.com>
2026-05-02 22:40:09 -0400

    Initial commit

---
+++ .luacheckrc
@@ -0,0 +1,16 @@
+ignore = {
+  '631', -- max_line_length (managed by stylua)
+}
+
+globals = {
+  'vim',
+}
+
+read_globals = {
+  'describe',
+  'it',
+  'assert',
+  'before_each',
+  'after_each',
+  'pending',
+}
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      eachSystem = lib.flip lib.mapAttrs (
+        lib.genAttrs (import systems) (system: nixpkgs.legacyPackages.${system})
+      );
+    in
+
+    {
+      devShells = eachSystem (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.just
+              pkgs.lua-language-server
+              pkgs.luajitPackages.luacheck
+              pkgs.luajitPackages.vusted
+              pkgs.stylua
+            ];
+          };
+        }
+      );
+    };
+}
